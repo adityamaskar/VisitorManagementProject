8d3d04924489d15cf0f8d5443f433d06d98585b2
Aditya Maskar <amaskar1291@gmail.com>
2024-02-14 00:37:51 +0530

    Made some more changes

--- src/main/java/com/visitorproject/API/VisitorExceptionController.java
+++ src/main/java/com/visitorproject/API/VisitorExceptionController.java
@@ -2,13 +2,25 @@ package com.visitorproject.API;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class VisitorExceptionController {
     @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<Object> exception(RuntimeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public RuntimeException exception(RuntimeException exception) {
+//        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return exception;
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public ResponseEntity<Object> exception(BadCredentialsException exception) {
+        return new ResponseEntity<>("Invalid Username or Password, Please enter correct password", HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> exception(Exception exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
     }
 }
